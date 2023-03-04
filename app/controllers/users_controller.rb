e1d87a5f2392a3887e1a26390449461246f6f9b2
Walter <walter.clark24@yahoo.com>
2023-03-04 18:23:43 -0500

    routes before seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,45 @@
 class UsersController < ApplicationController
+    rescue_from Active::RecordNotFound, with: :user_not_found
+
     def index 
         users = User.all 
         render json: users 
+    end
+
+    def show
+        render json: User.find(params[:id]), status: :ok
+    end
+
+    def create
+        render json: User.create(user_params), status: :created  
+    end
+
+    def update
+        users = User.find(params[:id])
+        render json = users.update(user_params), status: :ok
+    end
+
+
+    def destroy
+        users = User.find(params[:id])
+        users.destroy
+        head :no_content
+    end
+
+
+
+
+private 
+
+    def user_params
+        params.permit(:description, :price, :city, :state, :num_bedrooms, :num_bathrooms, :image, :host_id)
+    end
+    
+    def user_not_found
+        render json: { error: "User not found"}, status: :not_found
+    end
+
+    def invalid(error)
+        render json: { error: error.message}, status:422
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,13 @@ require 'faker'
 puts "Seeding data..."
 Faker::Config.locale = :en
 # listings 
-
 Listing.destroy_all
 User.destroy_all
 Host.destroy_all
 Review.destroy_all
 
-Listing.create!(
+
+Listing.create(
     description: "This peaceful cottage has enough room for the whole family and is only three minutes from Marginal Way! Despite being so close to town, you’ll find yourself nestled away against a classic Maine woodland for plenty of comfort and privacy. Gather in the open full kitchen to cook a wonderful meal, or enjoy your favorite novel and some vitamin D in the sunroom. The kids will find all the yard space and nature they’ll need to have all the adventure they can stand.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -21,7 +21,7 @@ Listing.create!(
     host_id: Faker::Number.within(range: 1..10)
     ) 
 
-Listing.create!(
+Listing.create(
     description: "Honeycrisp Cottage a Tiny Timber Frame is a bright, energy-efficient dwelling nestled on 9 acres overlooking a beautiful forested mountain & trails to explore along a brook. A peaceful getaway retreat equipped with a living room, a king bed, a loft with a queen bed, a full kitchen, & a full bathroom.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -31,7 +31,7 @@ Listing.create!(
     image: "https://a0.muscache.com/im/pictures/cc2be89b-da4f-4ada-90e7-84e092d11a44.jpg?im_w=720", 
     host_id: Faker::Number.within(range: 1..10)) 
 
-Listing.create!(
+Listing.create(
     description: "Honeycrisp Cottage a Tiny Timber Frame is a bright, energy-efficient dwelling nestled on 9 acres overlooking a beautiful forested mountain & trails to explore along a brook. A peaceful getaway retreat equipped with a living room, a king bed, a loft with a queen bed, a full kitchen, & a full bathroom.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -41,7 +41,7 @@ Listing.create!(
     image: "https://a0.muscache.com/im/pictures/cc2be89b-da4f-4ada-90e7-84e092d11a44.jpg?im_w=720", 
     host_id: Faker::Number.within(range: 1..10)) 
 
-Listing.create!(
+Listing.create(
     description: "The Glen House at Sunday River is the ultimate year-round luxury estate home, spa, and entertainment destination wrapped into one magnificent vacation rental estate. It's privately nestled into the most picturesque spot on the Sunday River Golf Course in the Mahoosuc Mountains of Maine. It's ideal for extended family vacations, reunions, special occasions, holidays, and retreats.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -51,7 +51,7 @@ Listing.create!(
     image: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-38883929/original/c817c05d-b2e6-48ff-a699-d85a5bc61a55.jpeg?im_w=720", 
     host_id: Faker::Number.within(range: 1..10)) 
 
-Listing.create!(
+Listing.create(
     description: "Enjoy the pure waters of the Adirondacks at this private lakefront home. The house sits on one acre of private land and is only one of the few properties with a private large yard, walk-in sandy beach, and a six-person outdoor hot tub! It is a perfect year-round vacation spot.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -61,7 +61,7 @@ Listing.create!(
     image: "https://a0.muscache.com/im/pictures/miso/Hosting-39945855/original/5cc2979f-6380-4f45-87c8-667a344c63a1.jpeg?im_w=720", 
     host_id: Faker::Number.within(range: 1..10)) 
 
-Listing.create!(
+Listing.create(
     description: "Enjoy the pure waters of the Adirondacks at this private lakefront home. The house sits on one acre of private land and is only one of the few properties with a private large yard, walk-in sandy beach, and a six-person outdoor hot tub! It is a perfect year-round vacation spot.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -72,7 +72,7 @@ Listing.create!(
     host_id: Faker::Number.within(range: 1..10)) 
 
 
-Listing.create!(
+Listing.create(
     description: "Unique experience, secluded.
     Enjoy a weekend or a few days eco-friendly retreat in an architectural, geometric masterpiece on 30 preserved acres just minutes from all that Rhinebeck and the Hudson Valley have to offer.
     The house is an open plan, and though it has zero bedrooms, it can sleep 3!", 
@@ -84,7 +84,7 @@ Listing.create!(
     image: "https://a0.muscache.com/im/pictures/miso/Hosting-21409981/original/a8fa243d-dac8-4238-93e5-f7aa33072ff8.jpeg?im_w=720", 
     host_id: Faker::Number.within(range: 1..10)) 
 
-Listing.create!(
+Listing.create(
     description: "Architectural masterpiece (b. 1961) w/ spectacular views and privacy. A soaring ceiling and a wall of glass overlooking the dunes, the ocean and the bay. Beneath, the enormous master suite looks out to the dunes on one side and the pool deck on the other. Across the pool deck are 2 guest cabana bedrooms and a bathroom. Cooling ocean breezes flow day and night in this sophisticated, relaxed compound. Multiple work areas in the home.", 
     price: Faker::Number.between(from: 0, to: 500), 
     city: Faker::Address.city, 
@@ -100,7 +100,7 @@ Listing.create!(
 
 # users 
 5.times do 
-    User.create!(
+    User.create(
         name: Faker::Name.name, 
         user_name: Faker::Internet.unique.username, 
         email: Faker::Internet.unique.email, 
@@ -111,7 +111,7 @@ end
 
 # reviews 
 10.times do 
-    Review.create!(
+    Review.create(
         comment: Faker::Quote.yoda, 
         user_id: Faker::Number.within(range: 1..10), 
         listing_id: Faker::Number.within(range: 1..10)
@@ -119,7 +119,7 @@ end
 end 
 
 5.times do 
-    Host.create!(
+    Host.create(
         name: Faker::Name.name
     )
 end
