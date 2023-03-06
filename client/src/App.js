66f425319a9b0ea7d5a95c14ba268c4524eb0a3c
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-03-06 13:14:53 -0500

    login and signup form ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
+import {Routes, Route} from "react-router-dom"
+import Login from './components/Login'
+import HomeList from './components/HomeList'
+import Signup from './components/Signup'
 
 function App() {
  
 
   return (
     <div>
-      <h1>App</h1>
+      <Routes>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<HomeList/>}/>
+      </Routes>
+  
     </div>
   );
 }
