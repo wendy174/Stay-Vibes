fb2f8575f72ea67826aece8e3acbf09aea6d035d
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-03-07 12:20:01 -0500

    Modal created and working

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,27 @@
 import { React, useState } from 'react'
 import { stack as Menu } from 'react-burger-menu'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import "../index.css"
 import Search from './Search';
+import Logo from './images/Logo.png'
 
 
 export default function NavBar() {
   const [menuState, setMenuState] = useState(false)
-
+let navigate = useNavigate()
   const handleOpen = () => {
       const newState = !menuState
       setMenuState(newState);
   }
 
+  function handleClick() {
+    navigate('/')
+  }
 return (
   <div className='header'>
+    <div onClick={handleClick}>
+      <img className='logo' src={Logo} alt='logo' onClick={handleOpen}/>
+    </div>
      <Search/>
     <Menu right > 
         <NavLink to="/">Home</NavLink>
@@ -25,8 +32,6 @@ return (
         <NavLink to="/login">Log Out</NavLink>
         
     </Menu>
-    <h1 className='stay-vibes' onClick={handleOpen}>Stay Vibes</h1>
-   
   </div>
 )
 }
