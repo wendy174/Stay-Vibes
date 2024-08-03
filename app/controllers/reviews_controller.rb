b7729f9949d664c0804e315adb5e1510262c4f2e
wendy <mizukilee@gmail.com>
2024-08-02 21:45:40 -0400

    cors config resolved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -46,7 +46,7 @@ private
     end
 
     def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = 'http://localhost:4000'
+        headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
         headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
     end
